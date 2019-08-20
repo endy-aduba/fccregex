6e4d47bf0e8cf70ad885d5f57ae515416877e70f
endy <endyaduba@gmail.com>
2019-08-20 15:08:47 +0100

    export functionality

---
+++ export.js
@@ -0,0 +1,4 @@
+// When we want some code - a function, or a variable - to be usable in another file, we must export it in order to import it into another file.
+
+// export { capitalizeString } //How to export functions.
+// export const foo = "bar"; //How to export variables.
---
+++ import.js
@@ -0,0 +1,4 @@
+// to import everything from a file, use the import * syntax
+// it takes this form 
+// import * as object_with_name_of_your_choice from "file_path_goes_here"
+// object_with_name_of_your_choice.imported_function
